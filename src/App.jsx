723e5a6428d1a2b352c54b509e945cc624685bc6
Temitope Olawuni <olawunitemitope68@gmail.com>
2024-06-26 13:33:29 +0100

    mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+ import Navbar from "./components/NavBar"
+import Model from "./components/Model"
+import Features from "./components/Features"
+import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer" 
 
 import * as Sentry from '@sentry/react';
 
 const App = () => {
   return (
     <main className="bg-black">
-      <Navbar />
+      <Navbar/>
       <Hero />
-      <Highlights />
+      <Highlights/>
       <Model />
       <Features />
       <HowItWorks />
