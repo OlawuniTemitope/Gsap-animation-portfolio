6b84710c88003721a45ccfc8ccdab412dc841e4f
Temitope Olawuni <olawunitemitope68@gmail.com>
2024-06-27 07:30:56 +0100

    chsnges in app.jsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -103,7 +103,8 @@ const Model = () => {
             <div className="flex-center">
               <ul className="color-container">
                 {models.map((item, i) => (
-                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
+                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" 
+                  style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
                 ))}
               </ul>
 
